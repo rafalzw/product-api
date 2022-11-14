68800983f912d03b2249be8dfd845efd12eef232
Rafal Zw <zwierz.rafal@gmail.com>
2022-11-14 23:58:00 +0100

    Added validation to create and update dto's.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,13 @@
 import { ProductInterface } from '../../types/product';
+import { IsNotEmpty, IsNumber, IsString, NotEquals } from 'class-validator';
 
 export class CreateProductDto implements ProductInterface {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @NotEquals(0)
   price: number;
 }
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,7 +1,23 @@
 import { ProductInterface } from '../../types/product';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  NotEquals,
+} from 'class-validator';
 
 export class UpdateProductDto implements ProductInterface {
+  @IsNotEmpty()
+  @IsUUID(4, { each: true })
   id: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @NotEquals(0)
   price: number;
 }
