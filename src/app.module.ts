82e973e9f5e23d3997ae03400425e861524f0e01
Rafal Zw <zwierz.rafal@gmail.com>
2022-11-15 21:13:01 +0100

    Code cleaned up.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [DatabaseModule, ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
